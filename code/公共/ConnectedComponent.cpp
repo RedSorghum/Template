7effd996cf9567fb68c49e67089f2debf7ea8d5b
Eriri <741674999@qq.com>
2018-10-17 09:57:24 +0800

    111

---
+++ code/公共/ConnectedComponent.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define repb(i,s,t) for(int i=(s);i<=(t);++i)
+#define mst(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define mp make_pair
+typedef pair<int,int> pii;
+
+const int maxn = 1000;
+
+//原图点从1到n，使用init初始化点数n与图N
+//使用run运行，生成各个点所属的集合的标号fa
+//割点存储在cut中，桥存储在bridge中
+struct BCC{
+	BCC(){init(0,0);}
+	int n,t,dfn[maxn],low[maxn],fa[maxn],s[maxn];
+	vector<int>*N,cut;vector<pii> bridge;
+	void init(int _n,vector<int>*_N){
+		n=_n;t=-1;N=_N;mst(dfn,0);mst(low,0);cut.clear();bridge.clear();}
+	void dfs(int u,int d,int p){
+		int sec=0,son=d>1;dfn[u]=low[u]=d;s[++t]=u;
+		for(auto v:N[u]){
+			if(!dfn[v]){
+				dfs(v,d+1,u);low[u]=min(low[u],low[v]);
+				if(low[v]>dfn[u])bridge.pb(mp(u,v));
+				if(low[v]>=dfn[u]&&++son==2)cut.pb(u);}
+			else if(v!=p||sec++)low[u]=min(low[u],dfn[v]);}
+		if(dfn[u]==low[u])do fa[s[t]]=u;while(s[t--]!=u);}
+	void run(){repb(i,1,n)if(!dfn[i])dfs(i,1,0);}};
+
+
+//原图点从1到n，使用init初始化点数n与图N
+//使用run运行，生成点联通分量数目m，各点联通分量存储在D[1,m]中
+//割点存储在cut中
+struct DCC{
+	DCC(){init(0,0);}
+	int n,t,m,dfn[maxn],low[maxn],s[maxn];
+	vector<int>*N,cut,D[maxn];
+	void init(int _n,vector<int>* _N){
+		n=_n;N=_N;t=-1;m=0;mst(dfn,0);mst(low,0);cut.clear();}
+	void dfs(int u,int d,int p){
+		int sec=0,son=d>1;dfn[u]=low[u]=d;s[++t]=u;
+		for(auto v:N[u]){
+			if(!dfn[v]){
+				dfs(v,d+1,u);low[u]=min(low[u],low[v]);
+				if(low[v]>=dfn[u]){
+					if(++son==2)cut.pb(u);
+					m+=1;D[m].clear();D[m].pb(u);
+					do D[m].pb(s[t]);while(s[t--]!=v);}}
+			else if(v!=p||sec++)low[u]=min(low[u],dfn[v]);}}
+	void run(){repb(i,1,n)if(!dfn[i])dfs(i,1,0);}};
--- code/公共/fastio.cpp
+++ code/公共/fastio.cpp
@@ -10,6 +10,20 @@ inline bool Cin(T &a){
 	do{(a*=10)+=*is&15;rd;}while(isdigit(*is)&&is!=ie);
 	if(s)a=-a;return 1;}
 
+template<class T>
+inline bool Cind(T &a){
+	a=0.;T b=1.;int s=0;bool neg=0;
+	if(is==ie)return 0;
+	while(!isgraph(*is)&&is!=ie)rd;
+	if(is==ie)return 0;
+	if(*is=='-')s=neg=1;else if(*is=='.')s=2;else a=*is&15,s=3;rd;
+	while(s&&isgraph(*is)&&is!=ie){
+		if(s==1)isdigit(*is)?(a*=10)+=*is&15,s=3:s=2;
+		else if(s==2)isdigit(*is)?a+=(b*=.1)*(*is&15),s=4:s=0;
+		else if(s==3)isdigit(*is)?(a*=10)+=*is&15:*is=='.'?s=2:s=0;
+		else if(s==4)isdigit(*is)?a+=(b*=.1)*(*is&15):s=0;
+		rd;}if(neg)a=-a;return 1;}
+
 inline int Gets(char a[]){
 	int p=0;
 	if(is==ie)return 0;
